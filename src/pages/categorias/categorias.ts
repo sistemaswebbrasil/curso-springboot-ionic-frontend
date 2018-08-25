289a2595b15487f9c1948a8013284f854d1e72d2
Adriano Faria <adriano.faria@outlook.com.br>
2018-08-25 12:41:30 -0300

    Correção do nome do arquivo categoria.dto.ts

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
-import { CategoriaDTO } from '../../models/Categoria.dto';
+import { CategoriaDTO } from '../../models/categoria.dto';
 import { API_CONFIG } from '../../config/api.config';
 
 /**
--- src/services/domain/categoria.service.ts
+++ src/services/domain/categoria.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
-import { CategoriaDTO } from "../../models/Categoria.dto";
+import { CategoriaDTO } from "../../models/categoria.dto";
 import { Observable } from "rxjs/Rx";
 
 @Injectable()
