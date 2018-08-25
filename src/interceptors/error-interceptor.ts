41451079968953e8bc4a2da2a92afb5c31f5d959
Adriano Faria <adriano.faria@outlook.com.br>
2018-08-25 15:57:07 -0300

    Tratamento padrao para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,12 @@ import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { StorageService } from "../services/storage.service";
+import { AlertController } from "ionic-angular";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage:StorageService){
+    constructor(public storage:StorageService,public alertCtrl : AlertController){
 
     }
 
@@ -26,8 +27,15 @@ export class ErrorInterceptor implements HttpInterceptor {
                 console.log(errorObj);
 
                 switch(errorObj.status){
+                    case 401:
+                    this,this.handle401();
+                    break;
                     case 403:
                     this.handle403();
+                    break;
+
+                    default:
+                    this.handleDefaultError(errorObj);
                 }
 
                 return Observable.throw(errorObj);
@@ -36,6 +44,35 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     handle403(){
         this.storage.setLocalUser(null);
+    }
+
+    handle401(){
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha de autenticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons:[
+                {
+                    text: 'OK'
+                }
+            ]
+        });
+        alert.present();
+    } 
+    
+    handleDefaultError(errorObj){
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': '+ errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons:[
+                {
+                    text: 'OK'
+                }
+            ]
+        });
+        alert.present();        
+
     }
 }
 
