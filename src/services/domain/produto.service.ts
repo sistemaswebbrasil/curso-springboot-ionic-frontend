ea7348c9f277ff420fbaa3361b3b9e8c700291e8
Adriano Faria <adriano.faria@outlook.com.br>
2018-08-28 21:39:23 -0300

    Carregando imagens dos produtos

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -8,7 +8,12 @@ export class ProdutoService {
   constructor(public http: HttpClient) {
   }
 
-  findByCategoria(categoria_id : string) {
+  findByCategoria(categoria_id: string) {
     return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+  }
+
+  getSmallImageFromBucket(id: string): Observable<any> {
+    let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
+    return this.http.get(url, { responseType: 'blob' });
   }
 }
