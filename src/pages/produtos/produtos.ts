96474b137556d4e7dcf22773dd59c0865304f107
Adriano Faria <adriano.faria@outlook.com.br>
2018-09-01 19:43:30 -0300

    Infinite scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -18,7 +18,9 @@ import { API_CONFIG } from '../../config/api.config';
 })
 export class ProdutosPage {
 
-  items: ProdutoDTO[];
+  items: ProdutoDTO[] = [];
+
+  page: number = 0;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
@@ -30,21 +32,25 @@ export class ProdutosPage {
     this.loadData();
   };
 
-  loadData(){
+  loadData() {
     let categoria_id = this.navParams.get('categoria_id')
     let loader = this.presentLoading();
-    this.produtoService.findByCategoria(categoria_id)
+    this.produtoService.findByCategoria(categoria_id, this.page, 10)
       .subscribe(response => {
-        this.items = response['content'];
+        let start = this.items.length;
+        this.items = this.items.concat(response['content']);
+        let end = this.items.length -1;
         loader.dismiss();
-        this.loadImageUrls();
+        console.log(this.page);
+        console.log(this.items);
+        this.loadImageUrls(start,end);
       }, error => {
         loader.dismiss();
-       });    
+      });
   }
 
-  loadImageUrls() {
-    for (var i = 0; i < this.items.length; i++) {
+  loadImageUrls(start: number, end: number) {
+    for (var i = start; i <=end; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(response => {
@@ -54,8 +60,8 @@ export class ProdutosPage {
     }
   };
 
-  showDetail(produto_id : string){
-    this.navCtrl.push('ProdutoDetailPage',{produto_id:produto_id});
+  showDetail(produto_id: string) {
+    this.navCtrl.push('ProdutoDetailPage', { produto_id: produto_id });
   }
 
   presentLoading() {
@@ -67,10 +73,20 @@ export class ProdutosPage {
   }
 
   doRefresh(refresher) {
+    this.page = 0;
+    this.items =[];
     this.loadData();
     setTimeout(() => {
       refresher.complete();
     }, 1000);
-  }  
+  }
+
+  doInfinite(infiniteScroll) {
+    this.page++;
+    this.loadData();
+    setTimeout(() => {
+      infiniteScroll.complete();
+    }, 1000);
+  }
 
 }
